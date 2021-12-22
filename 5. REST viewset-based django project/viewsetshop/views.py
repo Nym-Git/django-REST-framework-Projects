976965e6d46969b5daa03f8f3d93057733187cb6
nym <codelib0@gmail.com>
2021-12-22 14:09:39 +0530

    ViewSet based :: (CRUD functionlity in one function)

---
+++ 5. REST viewset-based django project/viewsetshop/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import Http404
+from .serializers import productSerializer
+from .models import Item
+
+from rest_framework.views import APIView
+from rest_framework import generics,mixins, serializers
+from rest_framework import viewsets
+
+
+# model-view-set      {all operation:- primery-key based and non-primery key based}...
+class product_CRUD(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = productSerializer
